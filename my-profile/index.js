454914a5c606e69672ad9a4bfd5f1423d3a77768
jmark <johnmarksaberola2@gmail.com>
2026-08-02 16:35:35 +0800

    Add files via upload

--- my-profile/index.js
+++ my-profile/index.js
@@ -1,10 +1,15 @@
-let firstname = "John Mark";
-let lastname = "Saberola";
-const imgsrc = "https://avatars.githubusercontent.com/u/240685117?s=400&u=ac8edd652276549c7c8adbdffa5dcab053f8049e&v=4";
+function calculate(a, b, operator) {
+  const operations = {
+    "+": a + b,
+    "-": a - b,
+    "*": a * b,
+    "/": b ? a / b : "Division by zero is not allowed"
+  };
 
+  return operations[operator] ?? "Invalid operator";
+}
 
-document.getElementById("content").innerHTML = `
-    <h1>My profile</h1>
-    <p>My name is ${firstname} ${lastname}</p>
-    <img src="${imgsrc}" alt="profile picture goes here">
-`;
+console.log(calculate(9, 5, "+"));
+console.log(calculate(8, 5, "-"));
+console.log(calculate(7, 5, "*"));
+console.log(calculate(0, 0, "/"));
